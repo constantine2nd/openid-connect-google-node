63ce146f5da96df56927a94055bacb3b706ed2e9
Marko Milić <marko@tesobe.com>
2023-01-31 14:41:52 +0100

    Add picture  to the user page

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,12 @@ const express = require('express');
 const router = express.Router();
 const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
 
+// middleware that is specific to this router
+router.use((req, res, next) => {
+  console.log('User: ', req.user)
+  next()
+})
+
 router.get('/', ensureLoggedIn('/'), (req, res) => {
   res.render('users', { user: req.user });
 });
